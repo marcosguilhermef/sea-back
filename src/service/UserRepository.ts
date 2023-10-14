da166bf63be16e56a819af23f2ef22e4587fb1e7
marcosguilhermef <marcosguilhermefh@gmail.com>
2023-10-14 11:29:57 -0300

    melhoria

--- src/service/UserRepository.ts
+++ src/service/UserRepository.ts
@@ -4,6 +4,7 @@ import { User } from "@prisma/client";
 /**
  * A criação de novas classes dependerá da necessidade da sobrescrição de métodos.
  */
+
 export class UserRepository extends BaseRepository<User>{
     constructor(key: string) {
         super(key)
